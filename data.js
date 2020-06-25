0d7e666c4064962cd5a403e8b0cd465a7bca8733
João Brosin <joao.brosin@gmail.com>
2020-06-25 16:03:54 -0300

    dynamic cards and descriptions added

---
+++ data.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: "starter",
+        image: "https://image.flaticon.com/icons/svg/3048/3048425.svg",
+        title: "Leia",
+        description: "Alugue qualquer livro sem sair de casa."
+    },
+    {
+        id: "launchbase",
+        image: "https://image.flaticon.com/icons/svg/1992/1992617.svg",
+        title: "Ganhe dinheiro",
+        description: "Empreste seus livros a um leitor e receba por isso."
+    },
+    {
+        id: "gostack",
+        image: "https://image.flaticon.com/icons/svg/2919/2919670.svg",
+        title: "Seja membro",
+        description: "Tenha acesso a ferramentas exclusivas."
+    }
+]
+
+
--- server.js
+++ server.js
@@ -2,22 +2,29 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-// const videos = require("./data")
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res){
-    return res.render("index") 
+    const facePage = {
+        title: "RentBooks",
+        description: 'Nós somos apaixonados por livros e temos o compromisso de facilitar o acesso de pessoas aos seus livros preferidos. Por isso, a <a class="links" href="index.html">RentBooks&copy;</a> criou a maior plataforma de empréstimos de livro do Brasil, onde você pode emprestar livros de qualquer lugar do país, sem precisar sair de casa.'
+    }
+
+    return res.render("index", {facePage: facePage}) 
 })
 
 server.get("/content", function(req, res){
-    return res.render("content")
+    return res.render("content", {items: videos})
 })
 
 server.listen(5000, function(){
